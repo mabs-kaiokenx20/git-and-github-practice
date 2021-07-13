544d9241e1eb5e7be2dbb424140a5a051e52ef17
Martin <martin.totari@gmail.com>
2021-07-13 11:55:31 -0500

    algebra lineal

---
+++ algebra lineal.py
@@ -0,0 +1,12 @@
+import numpy as np
+from math import pi
+
+
+a = np.array([[1,23,-5],[32,0.45,-90],[45,89,pi]])
+
+b = np.array([pi, 34, -90])
+
+
+solution = np.linalg.solve( a , b )
+
+print(solution)
